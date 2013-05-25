1a257a18a5c3e6578d575917ddf8e966c935cb1c
zhongbeyond <zhongyijun48729730@gmail.com>
2013-05-25 13:58:42 +0800

    modify function.php

--- functions.php
+++ functions.php
@@ -12,21 +12,21 @@ function twtwFFWD_navigation() {
 	?><script type="text/javascript">(function(){var e=document.getElementById("site-navigation"),t,n;if(!e)return;t=e.getElementsByTagName("h3")[0];n=e.getElementsByTagName("ul")[0];if(!t)return;if(!n||!n.childNodes.length){t.style.display="none";return}t.onclick=function(){if(-1==n.className.indexOf("nav-menu"))n.className="nav-menu";if(-1!=t.className.indexOf("toggled-on")){t.className=t.className.replace(" toggled-on","");n.className=n.className.replace(" toggled-on","")}else{t.className+=" toggled-on";n.className+=" toggled-on"}}})()</script><?php
 }
 
-// function twentytwelve_content_nav( $html_id ) {
-// 	global $wp_query;
+function twentytwelve_content_nav( $html_id ) {
+	global $wp_query;
 
-// 	$html_id = esc_attr( $html_id );
+	$html_id = esc_attr( $html_id );
 
-// 	if ( $wp_query->max_num_pages > 1 ) : ?>
-// 		<nav id="<?php echo $html_id; ?>" class="navigation" role="navigation">
-// 			 <?php wp_pagenavi(); ?>
-// 		</nav><!-- #<?php echo $html_id; ?> .navigation -->
-// 	<?php endif;
-// }
+	if ( $wp_query->max_num_pages > 1 ) : ?>
+		<nav id="<?php echo $html_id; ?>" class="navigation" role="navigation">
+			 <?php wp_pagenavi(); ?>
+		</nav><!-- #<?php echo $html_id; ?> .navigation -->
+	<?php endif;
+}
 
 add_action('wp_enqueue_scripts', 'twtwFFWD_queuing', 11 );
 add_action('twentytwelve_credits', 'twtwFFWD_credits' );
 add_action('wp_footer', 'twtwFFWD_navigation' );
 
-//add_action('twentytwelve_content_nav_new', 'twentytwelve_content_nav' );
+add_action('twentytwelve_content_nav_new', 'twentytwelve_content_nav' );
 ?>
