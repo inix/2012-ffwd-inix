cdb63c8b434383f0857056346585962fa5a2a5e9
zhongbeyond <zhongyijun48729730@gmail.com>
2013-05-31 16:41:13 +0800

    debug read link

--- functions.php
+++ functions.php
@@ -16,17 +16,3 @@ add_action('wp_enqueue_scripts', 'twtwFFWD_queuing', 11 );
 add_action('twentytwelve_credits', 'twtwFFWD_credits' );
 add_action('wp_footer', 'twtwFFWD_navigation' );
 ?>
-
-/* <?php */
-/* // Remove the ... from excerpt and change the text */
-/* function change_excerpt_more() */
-/* { */
-/*   function new_excerpt_more($more) */
-/*   { */
-/* 	// Use .read-more to style the link */
-/* 	return '<span class="read-more"> <a href="' . get_permalink($post->ID) . '">Continue Reading &raquo;</a></span>'; */
-/*   } */
-/*   add_filter('excerpt_more', 'new_excerpt_more'); */
-/* } */
-/* add_action('after_setup_theme', 'change_excerpt_more'); */
-/* ?> */
