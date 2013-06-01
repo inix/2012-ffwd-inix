89254928c01b2f26685468d2266a527f92baff29
zhongbeyond <zhongyijun48729730@gmail.com>
2013-06-01 13:17:30 +0800

    comment out excerpt length

--- functions.php
+++ functions.php
@@ -29,9 +29,10 @@ function change_excerpt_more()
 }
 add_action('after_setup_theme', 'change_excerpt_more');
 
-// Changing excerpt length
-function new_excerpt_length($length) {
-  return 35;
-}
-add_filter('excerpt_length', 'new_excerpt_length');
+/* // Changing excerpt length */
+/* function new_excerpt_length($length) { */
+/*   return 35; */
+/* } */
+/* add_filter('excerpt_length', 'new_excerpt_length'); */
+
 ?>
